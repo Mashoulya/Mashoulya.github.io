fc21f56e1ae573058377cbf2367cb91975b2a1e1
Maria <121291247+Mashoulya@users.noreply.github.com>
2024-07-15 22:25:12 +0200

    Taris
    Création de la page des tarifs

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll(".service-type button").forEach(function(button){
+//     button.addEventListener('click', function(){
+//         let overlay = this.parentNode.querySelector('.overlay');
+//         overlay.style.display = 'flex';
+//     });
+// });
+
+// document.querySelectorAll('.overlay button').forEach(function(button) {
+//     button.addEventListener('click', function() {
+//         let overlay = this.closest('.overlay');
+//         overlay.style.display = 'none';
+//     });
+// });
