78df4d19c357493281cb36d3ecd4e6ebcd2acafb
Maria <121291247+Mashoulya@users.noreply.github.com>
2024-08-01 19:54:34 +0200

    Formular

--- script.js
+++ script.js
@@ -23,5 +23,45 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+    // Vérification du formulaire
+
+    let contactForm = document.getElementById('contactForm');
+    let formError = document.getElementById('formError');
+
+    contactForm.addEventListener('click', function(event){
+        let lastName = document.getElementById('lastName').value.trim();
+        let firstName = document.getElementById('firstName').value.trim();
+        let email = document.getElementById('email').value.trim();
+        let tel = document.getElementById('tel').value.trim();
+        let msg = document.getElementById('message').value.trim();
+
+        formError.textContent = '';
+
+        let isValid = true;
+
+        // validation du nom
+        let nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$/;
+
+        if (lastName.length < 2 || lastName > 30 ){
+            formError.textContent += 'Le nom doit comporter entre 2 et 30 caractères';
+            isValid = false;
+        }else if(!nameRegex.test(lastName)){
+            formError.textContent = 'Le nom est invalide (utilisez uniquement des lettres et des tirets)';
+            isValid = false;
+        }
+
+        // if (firstName.length < 2){
+        //     formError.textContent = 'Le prénom est trop court';
+        //     isValid = false;
+        // }
+        
+let phoneRegex = /^0[1-9][0-9]{8}$/;
+let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    });
+
+
+
+
 });
    
